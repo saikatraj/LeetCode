3f68333f1f847acdd5180900f541e23ffa57250f
saikatraj <62927638+saikatraj@users.noreply.github.com>
2022-09-01 02:21:26 +0530

    Add files via upload

---
+++ 12. Integer to Roman.py
@@ -0,0 +1,8 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        m = ["", "M", "MM", "MMM"]
+        return m[num//1000] + c[(num%1000)//100] + x[(num%100)//10] + i[num%10]
+
